92a74ba4efc0a4a2c440796632c6c79900f0823a
KevinPaico <141683583+KevinPaico@users.noreply.github.com>
2023-10-03 18:47:49 -0300

    Create Conexion.java

---
+++ VacunAR23/src/vacunar23/acceso_a_datos/Conexion.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vacunar23.acceso_a_datos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Equipo
+ */
+public class Conexion {
+
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "vacunar23";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+
+    private static Connection conexion;
+
+    private Conexion() {
+    }
+
+    public static Connection getConection() {
+
+        if (conexion == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar el driver");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos");
+            }
+        }
+        return conexion;
+    }
+}
