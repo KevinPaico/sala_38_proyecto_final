3aea051ada96c16a2780aeec7376d07d7c6aeece
SchiiA <santischiavone2002@gmail.com>
2023-10-03 18:41:23 -0300

    x

---
+++ VacunAR23/src/vacunar23/entidades/Ciudadano.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vacunar23.entidades;
+
+/**
+ *
+ * @author User
+ */
+public class Ciudadano {
+    private int dni;
+    private String nombreCompleto;
+    private String email;
+    private String celular;
+    private String patologia = null;
+    private String ambitoTrabajo;
+}
---
+++ VacunAR23/src/vacunar23/entidades/Vacuna.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vacunar23.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author User
+ */
+public class Vacuna {
+    private int nroSerieDosis;
+    private String marca;
+    private double medida;
+    private Date fechaCaduca;
+    private boolean colocada = false;
+}
